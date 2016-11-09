6ce6b4bd26f24eb89d18c5ad745bf3c828408677
andriufus <skruzdzius@inbox.lt>
2016-11-09 19:10:48 +0200

    Added gulp packages and created watch tasks for scss and js

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var minify = require('gulp-minify');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+
+
+gulp.task('sass', function () {
+    return gulp.src('./app/Resources/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./web/assets/css'));
+});
+
+gulp.task('sass-min', function () {
+    return gulp.src('./app/Resources/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./web/assets/css'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+
+        // Grab all js scripts
+        './app/Resources/js/**/*.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./web/assets/js'))
+        .pipe(minify())
+        .pipe(gulp.dest('./web/assets/js'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./app/Resources/scss/**/*.scss', ['sass','sass-min']);
+    gulp.watch('./app/Resources/js/**/*.js', ['js']);
+});
