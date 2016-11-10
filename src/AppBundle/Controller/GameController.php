895b030004b06e3896c1d7c287d89084a7933e70
Antanas Gecas <antanas.gecas@gmail.com>
2016-11-10 21:06:30 +0200

    game init created

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -4,21 +4,26 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-
-/**
- * @Route("/game")
- */
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Fact;
 
 class GameController extends Controller
 {
     /**
-     * @Route("/init", name="homepage")
+     * @Route("/game/init", name="gameInit")
      */
     public function initGameAction()
-    {
+            {
+
+                $count = $this
 
 
+                $fact = $this->getDoctrine()
+                    ->getRepository('AppBundle:Fact')
+                    ->findAll();
+                var_dump($fact);
+
+return new Response;
 
     }
 }
--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -4,18 +4,25 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
+        return new Response('<h1>veikia game home</h1>');
+
+
+
+
+
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+       // return $this->render('default/index.html.twig', [
+      //      'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+       // ]);
     }
 }
