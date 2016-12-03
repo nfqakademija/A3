0afcdafdad1445efa5d7eb87bcd89ffd5e153d2c
Antanas Gecas <antanas.gecas@gmail.com>
2016-12-03 16:17:11 +0200

    Edited Fixtures file

--- src/AppBundle/Controller/FactController.php
+++ src/AppBundle/Controller/FactController.php
@@ -4,15 +4,50 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Fact;
 
 class FactController extends Controller
 {
     /**
-     * @Route("/facts", name="facts")
+     *
+     * @Route("/facts/{id}", name="facts")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function displayAction()
+    public function getFactAction($id)
     {
-        return new Response('<h1>veikia game facts</h1>');
+
+        try {
+            $fact = $this->getDoctrine()
+                ->getRepository('AppBundle:Fact')->getFactDetails($id);
+        } catch (\Exception $e) {
+            throw $this->createNotFoundException("Fact  not found");
+        }
+
+        $date = $fact->getYear();
+        $month = $fact->getMonth();
+        $day = $fact->getDay();
+        $name = $fact->getName();
+        $description = $fact->getDescription();
+
+        if ($month == null) {
+            $date = $date . " m.";
+        } elseif ($day == null) {
+            $date = $date . " m. " . $month . " mėn. ";
+        } else {
+            $date = $date . " m. " . $month . " mėn. " . $day . " d.";
+        }
+
+
+        $response["fact_info"] = array(
+            "date" => $date,
+            "name" => $name,
+            "description" => $description
+        );
+
+
+        return $this->json($response);
     }
 }
